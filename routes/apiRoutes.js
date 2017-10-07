2fc7ffaffebeb2c7a98971768ad2b5c1fc9f1d2d
Tara <tara@nelsonwriting.com>
2017-10-07 19:27:08 -0400

    Post actually works now.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,12 @@
 module.exports = function(app) {
-    
-//Send static 'assets' folder files used by the html files to the server
-
-   //app.use(express.static('./app/assets'));
 
 //Display survey.html when user requests endpoint '/response'
 
     app.post("/response", function(req, res) {
-        res.send("Client sent me " + req.body.name);
+
+        var charName = req.body.name;
+
+        res.send("Client sent me " + charName);
         console.log("Respond, darn you!");
     });
 
