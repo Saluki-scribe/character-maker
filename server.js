9c7b519c9fe56b48975149d347e5267c5117da7e
Tara <tara@nelsonwriting.com>
2017-10-08 17:21:45 -0400

    Added connection option for Jaws database.

--- server.js
+++ server.js
@@ -18,12 +18,20 @@
 
 //Set up user verification to access MySQL database
 
-    var connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "charactersDB"
-    });
+    var connection;
+    if(process.env.JAWSDB_URL) {
+    //Heroku deployment
+        connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+    //local host
+        connection = mysql.createConnection({
+            root: 3000,
+            host: "localhost",
+            user: "root",
+            password: "",
+            database: "db_name",
+        });
+    };
 
 //Handle errors from MySQL
 
